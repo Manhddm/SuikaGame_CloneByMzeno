ad1bc40c3366b6cacd595fa02e1195a4833f89a8
ManhDucDong <manhducdongyb@gmail.com>
2026-08-13 01:27:08 +0700

    Refactor ComponentPool to improve code readability, add lifecycle hook methods, and enhance prefab management logic

--- Assets/MyGame/Pool/ComponentPool.cs
+++ Assets/MyGame/Pool/ComponentPool.cs
@@ -4,7 +4,8 @@ using Object = UnityEngine.Object;
 
 namespace MyGame.Pool
 {
-    public abstract class ComponentPool<T> : BaseObjectPool<T> where T : Component
+    public abstract class ComponentPool<T> : BaseObjectPool<T>
+        where T : Component
     {
         private readonly T _prefab;
         private readonly Transform _poolParentInactive;
@@ -15,16 +16,24 @@ namespace MyGame.Pool
             int defaultPoolSize = 20,
             int maxPoolSize = 100,
             bool dontDestroyOnLoad = false)
-            : base(collectionCheck, defaultPoolSize, maxPoolSize)
+            : base(
+                collectionCheck,
+                defaultPoolSize,
+                maxPoolSize)
         {
             if (prefab == null)
             {
-                throw new ArgumentNullException(nameof(prefab), $"{typeof(T).Name} prefab is missing.");
+                throw new ArgumentNullException(
+                    nameof(prefab),
+                    $"{typeof(T).Name} prefab is missing."
+                );
             }
 
             _prefab = prefab;
 
-            var container = new GameObject($"{_prefab.name} Inactive Pool");
+            var container =
+                new GameObject($"{_prefab.name} Inactive Pool");
+
             if (dontDestroyOnLoad)
             {
                 Object.DontDestroyOnLoad(container);
@@ -35,38 +44,79 @@ namespace MyGame.Pool
 
         protected override T Create()
         {
-            return Object.Instantiate(_prefab, _poolParentInactive);
+            T item = Object.Instantiate(
+                _prefab,
+                _poolParentInactive
+            );
+
+            return item;
         }
 
         protected override void OnCreate(T item)
         {
             item.gameObject.SetActive(false);
+            OnCreateComponent(item);
         }
 
         protected override void OnRent(T item)
         {
             item.transform.SetParent(null, true);
             item.gameObject.SetActive(true);
+
+            OnRentComponent(item);
         }
 
         protected override void OnReturn(T item)
         {
-            item.transform.SetParent(_poolParentInactive, false);
+            OnReturnComponent(item);
+
             item.gameObject.SetActive(false);
+            item.transform.SetParent(
+                _poolParentInactive,
+                false
+            );
         }
 
         protected override void OnDestroy(T item)
         {
-            if (item == null) return;
-            Object.Destroy(item.gameObject);
+            OnDestroyComponent(item);
+
+            if (item != null)
+            {
+                Object.Destroy(item.gameObject);
+            }
         }
 
         protected override void OnDispose()
         {
+            OnDisposeComponentPool();
+
             if (_poolParentInactive != null)
             {
-                Object.Destroy(_poolParentInactive.gameObject);
+                Object.Destroy(
+                    _poolParentInactive.gameObject
+                );
             }
+        }
+
+        protected virtual void OnCreateComponent(T item)
+        {
+        }
+
+        protected virtual void OnRentComponent(T item)
+        {
+        }
+
+        protected virtual void OnReturnComponent(T item)
+        {
+        }
+
+        protected virtual void OnDestroyComponent(T item)
+        {
+        }
+
+        protected virtual void OnDisposeComponentPool()
+        {
         }
     }
 }
