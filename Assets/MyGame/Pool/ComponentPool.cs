48be508a855173b1b5ed2eaa6db8f9f62c007685
ManhDucDong <manhducdongyb@gmail.com>
2026-08-13 01:01:06 +0700

    Add ComponentPool<T> class for pooling Unity Components; implement creation, renting, and return logic

---
+++ Assets/MyGame/Pool/ComponentPool.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+namespace MyGame.Pool
+{
+    public abstract class ComponentPool<T> : BaseObjectPool<T> where T : Component
+    {
+        private readonly T _prefab;
+        private readonly Transform _poolParentInactive;
+        protected ComponentPool(T prefab, bool collectionCheck = true, int defaultPoolSize = 20, int maxPoolSize = 100)
+            : base(collectionCheck, defaultPoolSize, maxPoolSize)
+        {
+            if (prefab == null)
+            {
+                throw new MissingReferenceException(
+                    $"{typeof(T).Name} prefab is missing."
+                );
+            }
+            _prefab = prefab;
+            _poolParentInactive = new GameObject($"{_prefab.name} Inactive Pool").transform;
+        }
+
+        protected override T Create()
+        {
+            return Object.Instantiate(_prefab, _poolParentInactive);
+        }
+
+        protected override void OnCreate(T item)
+        {
+            item.gameObject.SetActive(false);
+        }
+
+        protected override void OnRent(T item)
+        {
+            item.transform.SetParent(null, true);
+            item.gameObject.SetActive(true);
+        }
+
+        protected override void OnDestroy(T item)
+        {
+            if (item == null) return;
+            Object.Destroy(item.gameObject);
+        }
+
+        protected override void OnReturn(T item)
+        {
+            item.gameObject.SetActive(false);
+            item.transform.SetParent(_poolParentInactive, false);
+        }
+    }
+}
